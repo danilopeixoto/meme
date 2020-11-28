8e709531e5cc9fdc3519326cedfac45d80264d3d
Danilo Peixoto Ferreira <danilo.ferreira@inatel.br>
2020-11-28 10:08:38 -0300

    Expose login route

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -5,7 +5,10 @@ set -eu
 variables='${SERVER_PORT}
           ${API_HOSTNAME}
           ${API_PORT}
-          ${API_DOMAIN}'
+          ${API_DOMAIN}
+          ${CORE_HOSTNAME}
+          ${CORE_PORT}
+          ${CORE_VERSION}'
 
 envsubst "$variables" < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
